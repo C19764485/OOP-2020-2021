a41daa5983d816755f2f3d52127232f803c9e05a
C19764485 <c19764485@mytudublin.ie>
2021-02-18 09:35:06 +0000

    my version

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -121,45 +121,6 @@ public class Arrays extends PApplet {
         
     }
 
-    void drawAxis(float[] data, String[] horizLabels, int verticalIntervals, float vertDataRange, float border) {
-        stroke(200, 200, 200);
-        fill(200, 200, 200);
-
-        // Draw the horizontal azis
-        line(border, height - border, width - border, height - border);
-
-        float windowRange = (width - (border * 2.0f));
-        float horizInterval = windowRange / (data.length - 1);
-        float tickSize = border * 0.1f;
-
-        for (int i = 0; i < data.length; i++) {
-            // Draw the ticks
-            float x = border + (i * horizInterval);
-            line(x, height - (border - tickSize), x, (height - border));
-            float textY = height - (border * 0.5f);
-
-            // Print the text
-            textAlign(CENTER, CENTER);
-            text(horizLabels[i].substring(0, 3), x, textY);
-
-        }
-
-        // Draw the vertical axis
-        line(border, border, border, height - border);
-
-        float verticalDataGap = vertDataRange / verticalIntervals;
-        float verticalWindowRange = height - (border * 2.0f);
-        float verticalWindowGap = verticalWindowRange / verticalIntervals;
-        for (int i = 0; i <= verticalIntervals; i++) {
-            float y = (height - border) - (i * verticalWindowGap);
-            line(border - tickSize, y, border, y);
-            float hAxisLabel = verticalDataGap * i;
-
-            textAlign(RIGHT, CENTER);
-            text((int) hAxisLabel, border - (tickSize * 2.0f), y);
-        }
-    }
-
     public void draw() {
         background(0);
         switch (mode) {
